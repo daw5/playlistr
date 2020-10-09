76e1a1bb089f776851f900fcc282979dfca0cdb4
Dawson Ley <dawsonley@Dawsons-MacBook-Pro-2.local>
2020-10-09 15:29:31 -0700

    boilerplate with mongo connection

---
+++ src/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const User = require("./User.model");
+
+const connection = "mongodb://mongo:27017/mongo-test";
+
+const connectDb = () => {
+  return mongoose.connect(connection);
+};
+
+module.exports = connectDb;
