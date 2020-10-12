cd0f0841d4316daaa65007e9552b23d9f92a00c0
Dawson Ley <dawsonley@Dawsons-MacBook-Pro-2.local>
2020-10-11 18:11:50 -0700

    added socket io server

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ import bodyParser from "body-parser";
 import passport from "passport";
 
 const PORT = process.env.PORT;
+const SOCKET_PORT = process.env.SOCKET_PORT;
+
 const express = require("express");
+const http = require("http");
+const socket = require("socket.io");
 const app = express();
+const server = http.createServer();
+const io = socket(server);
 const connectDb = require("./database/connection");
 
 applyPassportStrategy(passport);
@@ -23,3 +29,5 @@ app.listen(PORT, function () {
     console.log("Mongo connected");
   });
 });
+
+server.listen(SOCKET_PORT, () => console.log(`Socket Server Running`));
