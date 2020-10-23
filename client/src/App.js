c29a5a4f29c848bdfc701c4de9693183d1b894cb
Dawson Ley <dawsonley@Dawsons-MacBook-Pro-2.local>
2020-10-23 00:52:24 -0700

    installed react helmet and included meta tag for material'

--- client/src/App.js
+++ client/src/App.js
@@ -3,34 +3,47 @@ import "./App.scss";
 import { Login, Messaging } from "./components";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { SocketService } from "./services";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   const socketService = new SocketService();
   socketService.authenticateSocket();
 
   return (
-    <div id="app">
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/messaging">Messaging</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/messaging">
-            <Messaging />
-          </Route>
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <HelmetProvider>
+      <div id="app">
+        <Router>
+          <div>
+            <Helmet>
+              <meta
+                name="viewport"
+                content="minimum-scale=1, initial-scale=1, width=device-width"
+              />
+            </Helmet>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/messaging">Messaging</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <Switch>
+            <Route path="/messaging">
+              <Messaging />
+            </Route>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </HelmetProvider>
   );
 }
 
