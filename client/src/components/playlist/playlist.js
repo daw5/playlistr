47823b04cb64d4e228402c23b3e11f49dadff53e
Dawson Ley <daw5@protonmail.com>
2020-11-03 17:50:41 -0800

    added buttons to layout

--- client/src/components/playlist/playlist.js
+++ client/src/components/playlist/playlist.js
@@ -1,4 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import { Player } from "..";
 
 import "./playlist.scss";
@@ -8,7 +10,17 @@ require("dotenv").config();
 export default function Playlist(props) {
   return (
     <div className="playlist-container">
-      <Player />
+      <div className="playlist">
+        <button className="back-button">
+          <ChevronLeftIcon fontSize="large" />
+        </button>
+        <Player />
+        <button className="forward-button">
+          {" "}
+          <ChevronRightIcon fontSize="large" />
+        </button>
+      </div>
+      <div className="chat-container"></div>
     </div>
   );
 }
