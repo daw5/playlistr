1604a541c49dab5cb240a7e779b24693430caeea
Dawson Ley <daw5@protonmail.com>
2020-11-10 05:52:27 -0800

    thumbnails are fetched when new track added

--- client/src/components/playlist-create/playlist-create.js
+++ client/src/components/playlist-create/playlist-create.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
-import { playlistService } from "../../services";
+import { playlistService, thumbnailService } from "../../services";
+import { Button, TextField } from "@material-ui/core";
 import { SortableGrid } from "..";
 
 import "./playlist-create.scss";
@@ -7,29 +8,45 @@ import "./playlist-create.scss";
 require("dotenv").config();
 
 export default function Playlist(props) {
-  const [playlist, setPlaylist] = useState({
-    title: "",
-    urls: [
-      "https://www.youtube.com/watch?v=q187sD7l3BM",
-      "https://www.youtube.com/watch?v=Ozw-IpWalJs",
-    ],
-  });
-  useEffect(() => {
-    createPlaylist();
-  }, []);
+  const [tracks, setTracks] = useState([]);
+  const [trackToAdd, setTrackToAdd] = useState("");
 
-  const createPlaylist = () => {
-    playlistService
-      .createPlaylist(playlist.title, playlist.urls)
-      .then((playlist) => {
-        console.log("playlist: ", playlist);
-        // navigate to newly created playlist
-      });
+  // const createPlaylist = () => {
+  //   playlistService
+  //     .createPlaylist(playlist.title, playlist.urls)
+  //     .then((playlist) => {
+  //       console.log("playlist: ", playlist);
+  //       // navigate to newly created playlist
+  //     });
+  // };
+
+  const addTrack = () => {
+    const thumbnailUrl = thumbnailService.getThumbnailURL(trackToAdd);
+    setTracks([...tracks, { url: trackToAdd, thumbnailUrl }]);
   };
+
   return (
     <div className="playlist-create-container">
-      <p>I am playlist create</p>
-      <SortableGrid items={playlist.urls} />
+      <div className="inputContainer">
+        <TextField
+          onKeyDown={(evt) => evt.key === "Enter" && addTrack(evt)}
+          placeholder="Paste link to media here"
+          value={trackToAdd}
+          className="messageInput"
+          onChange={(evt) => setTrackToAdd(evt.target.value)}
+          InputProps={{
+            style: { color: "#fff" },
+          }}
+        />
+        <Button
+          className="sendMessageButton"
+          onClick={(evt) => addTrack(evt)}
+          variant="contained"
+        >
+          Send
+        </Button>
+      </div>
+      <SortableGrid tracks={tracks} setTracks={setTracks} />
     </div>
   );
 }
--- client/src/components/sortable-grid/sortable-grid.js
+++ client/src/components/sortable-grid/sortable-grid.js
@@ -1,49 +1,30 @@
 import React, { Component } from "react";
 import { sortableContainer, sortableElement } from "react-sortable-hoc";
 import ReactPlayer from "react-player";
+import { thumbnailService } from "../../services";
 import arrayMove from "array-move";
 import "./sortable-grid.scss";
 import "../player/player.scss";
 
 const SortableItem = sortableElement(({ value }) => (
-  <div className="sortable-item player-wrapper">
-    <ReactPlayer
-      className="react-player"
-      url={value}
-      width="100%"
-      height="100%"
-    />
-  </div>
+  <img src={value.thumbnailUrl} className="sortable-item" />
 ));
 
 const SortableContainer = sortableContainer(({ children }) => {
   return <div className="sortable-container">{children}</div>;
 });
 
-export default class SortableGrid extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: this.props.items,
-    };
-  }
-
-  onSortEnd = ({ oldIndex, newIndex }) => {
-    this.setState(({ items }) => ({
-      items: arrayMove(items, oldIndex, newIndex),
-    }));
+export default function SortableGrid(props) {
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    const previousTracks = props.tracks;
+    props.setTracks(arrayMove(previousTracks, oldIndex, newIndex));
   };
 
-  render() {
-    const { items } = this.state;
-
-    return (
-      <SortableContainer axis="xy" onSortEnd={this.onSortEnd}>
-        {items.map((value, index) => (
-          <SortableItem key={`item-${value}`} index={index} value={value} />
-        ))}
-      </SortableContainer>
-    );
-  }
+  return (
+    <SortableContainer axis="xy" onSortEnd={onSortEnd}>
+      {props.tracks.map((value, index) => (
+        <SortableItem key={`item-${index}`} index={index} value={value} />
+      ))}
+    </SortableContainer>
+  );
 }
