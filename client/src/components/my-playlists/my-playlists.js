012dedada503588a0892dab0a497ab3fca209ac2
Dawson Ley <daw5@protonmail.com>
2020-11-11 16:09:41 -0800

    scaffolding for playlist head done

--- client/src/components/my-playlists/my-playlists.js
+++ client/src/components/my-playlists/my-playlists.js
@@ -1,18 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { playlistService } from "../../services";
+import Button from "@material-ui/core/Button";
 import "./my-playlists.scss";
 
 export default function MyPlaylists(props) {
   const [playlists, setPlaylists] = useState([]);
   useEffect(() => {
-    setPlaylists(playlistService.getUserPlaylists());
+    playlistService.getUserPlaylists().then((playlists) => {
+      setPlaylists(playlists);
+      console.log("playlists: ", playlists);
+    });
   }, []);
   return (
     <div className="my-playlists-container">
-      <div className="playlist">
-        <div className="playlist-head"></div>
-        <div className="playlist-body"></div>
-      </div>
+      {playlists &&
+        playlists.map((playlist) => (
+          <div className="playlist">
+            <div className="playlist-head">
+              <div className="playlist-title-container">
+                <h2>{playlist.title}</h2>
+              </div>
+              <div className="playlist-functions-container">
+                <Button className="playlist-play-button" variant="contained">
+                  Play
+                </Button>
+                <Button className="playlist-edit-button" variant="contained">
+                  Edit
+                </Button>
+              </div>
+            </div>
+            <div className="playlist-body"></div>
+          </div>
+        ))}
     </div>
   );
 }
