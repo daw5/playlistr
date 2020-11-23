6b6d91ce86cdf5bb631b712a4bedf3a8c469a5f1
Dawson Ley <daw5@protonmail.com>
2020-11-22 18:33:32 -0800

    updated unique index so null is allowed

--- src/database/models/user.model.js
+++ src/database/models/user.model.js
@@ -2,7 +2,14 @@ import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
 const userSchema = new Schema({
-  username: { type: String, required: false, unique: true },
+  username: {
+    type: String,
+    required: false,
+    index: {
+      unique: true,
+      partialFilterExpression: { username: { $type: "string" } },
+    },
+  },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   status: {
