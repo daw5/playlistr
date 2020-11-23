d465fa61deb2cf9167201e9ff615bd3fcdb40c64
Dawson Ley <daw5@protonmail.com>
2020-11-22 17:55:07 -0800

    duplicate null usernames were causing issue, now it's not required

--- src/database/models/user.model.js
+++ src/database/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
 const userSchema = new Schema({
-  username: { type: String, default: null, unique: true },
+  username: { type: String, required: false, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   status: {
