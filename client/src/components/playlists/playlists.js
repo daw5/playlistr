3bbf3625ab7b15cfd1ed16016dbd544f0f27f4a9
Dawson Ley <daw5@protonmail.com>
2020-11-27 06:25:28 -0800

    flaming animation is properly correlated to loading state

--- client/src/components/playlists/playlists.js
+++ client/src/components/playlists/playlists.js
@@ -10,12 +10,14 @@ import "./playlists.scss";
 export default function Playlists(props) {
   const [playlists, setPlaylists] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [flaming, setFlaming] = useState(false);
   const [playlistToDelete, setPlaylistToDelete] = useState(null);
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
   const history = useHistory();
 
   useEffect(() => {
     props.socket ? getActivePlaylists() : getPlaylists();
+    setLoading(true);
   }, [props.socket]);
 
   const play = (playlist, track) => {
@@ -56,23 +58,25 @@ export default function Playlists(props) {
   };
 
   const getActivePlaylists = () => {
+    setFlaming(true);
     props.socket.on("get-active-playlists", function (data) {
       const uniquePlaylists = [
         ...new Map(data.map((playlist) => [playlist._id, playlist])).values(),
       ];
       setPlaylists([...uniquePlaylists]);
       setTimeout(() => {
-        setLoading(false);
-      }, 3000);
+        setFlaming(false);
+      }, 4999);
+      setLoading(false);
     });
     props.socket.emit("get-active-playlists", true);
   };
 
   return (
     <div
-      className={`playlists-container ${
-        loading && props.socket ? "hot" : undefined
-      } ${!loading && props.socket ? "fade-out" : undefined}`}
+      className={`playlists-container ${flaming ? "hot" : undefined} ${
+        !loading ? "fade-out" : undefined
+      }`}
     >
       {playlists &&
         playlists.map((playlist, index) => (
