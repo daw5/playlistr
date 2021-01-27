cb8791e96e8e29f04f3afe66ecf6ab7915400201
Dawson Ley <daw5@protonmail.com>
2021-01-26 16:32:55 -0800

    minor cleanup

--- src/store/config.js
+++ src/store/config.js
@@ -5,12 +5,4 @@ export const config = {
     secret: process.env.PASSPORT_SECRET,
     expiresIn: 10000,
   },
-  env: {
-    port: process.env.PORT,
-    mongoDBUri:
-      process.env.ENV === "prod"
-        ? "mongodb+srv://user:userTestingPassword@cluster0-yfpdy.mongodb.net/test?retryWrites=true&w=majority"
-        : "mongodb://localhost/test",
-    mongoHostName: process.env.ENV === "prod" ? "mongodbAtlas" : "localhost",
-  },
 };
